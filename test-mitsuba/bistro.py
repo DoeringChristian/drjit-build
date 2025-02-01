6da37d54198580e0501a2322f38e84f79c2427bb
Christian Döring <christian.doering@tum.de>
2025-02-02 00:13:26 +0100

    Added burnin

--- test-mitsuba/bistro.py
+++ test-mitsuba/bistro.py
@@ -2,10 +2,12 @@ import os
 from typing import Callable
 import mitsuba as mi
 import drjit as dr
+import time
+import tqdm
 
 mi.set_variant("cuda_ad_rgb")
 
-os.makedirs("out/bistro", exist_ok = True)
+os.makedirs("out/bistro", exist_ok=True)
 
 
 def func(scene: mi.Scene, seed: mi.UInt32) -> mi.TensorXf:
@@ -17,20 +19,39 @@ def func(scene: mi.Scene, seed: mi.UInt32) -> mi.TensorXf:
 frozen = dr.freeze(func)
 
 
-def run(name: str, scene: mi.Scene, n: int, func: Callable[[mi.Scene, mi.UInt32], mi.TensorXf])->list[mi.TensorXf]:
+def run(
+    name: str,
+    scene: mi.Scene,
+    func: Callable[[mi.Scene, mi.UInt32], mi.TensorXf],
+    n: int,
+    b: int = 3,
+) -> list[mi.TensorXf]:
     images = []
-    for i in range(n):
+    duration = 0
+    for i in tqdm.tqdm(range(b + n)):
+        if i >= b:
+            duration -= time.time()
+        dr.sync_thread()
         img = func(scene, mi.UInt32(i))
+        dr.sync_thread()
+        if i >= b:
+            duration += time.time()
         images.append(img)
         mi.util.write_bitmap(f"out/bistro/{name}{i}.exr", img)
-    return images
+    return images, duration / n
+
 
 scene = mi.load_file("data/bistro_singlebsdf/scene.xml")
 n = 10
+b = 3
 
-ref = run("normal", scene, n, func)
-res = run("frozen", scene, n, frozen)
+print("Normal:")
+ref, d_ref = run("normal", scene, func, n, b)
+print("Frozen:")
+res, d_res = run("frozen", scene, frozen, n, b)
 
 assert frozen.n_recordings < n
 for ref, res in zip(ref, res):
     assert dr.allclose(ref, res)
+
+print(f"normal: {d_ref}s, frozen: {d_res}s")
