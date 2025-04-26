f799f8d99d5630a59f09e59f788310bf7c7cca8d
Christian Döring <christian.doering@tum.de>
2025-04-26 10:37:28 +0200

    Fixed llvm

--- devenv.nix
+++ devenv.nix
@@ -1,21 +1,27 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
   # https://devenv.sh/packages/
   packages = with pkgs; [
-      git
-      gcc13
-      stdenv.cc.cc.lib
-      zlib
-      ninja
-      cmake
-      ccache
-      gdb
-      pkg-config
-      embree
-      # python311Full
-      llvmPackages_19.clang
-      cudaPackages.cudatoolkit
+    git
+    gcc13
+    stdenv.cc.cc.lib
+    zlib
+    ninja
+    cmake
+    ccache
+    gdb
+    pkg-config
+    embree
+    # python311Full
+    llvmPackages_19.clang
+    cudaPackages.cudatoolkit
+    llvm.lib
+    llvm.dev
   ];
 
   languages = {
@@ -24,37 +30,38 @@
       version = "3.11";
       venv.enable = true;
       venv.requirements = ''
-      pytest>=8.3.3,<9
-      numpy>=2.1.2,<3
-      tqdm>=4.67.1,<5
+        pytest>=8.3.3,<9
+        numpy>=2.1.2,<3
+        tqdm>=4.67.1,<5
       '';
     };
   };
 
   scripts = {
     configure-mitsuba.exec = ''
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S mitsuba3/ -B mitsuba3/build-mitsuba
+      cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S mitsuba3/ -B mitsuba3/build-mitsuba
     '';
 
     build-mitsuba.exec = ''
-    configure-mitsuba
-    cd mitsuba3/build-mitsuba
-    ninja
+      configure-mitsuba
+      cd mitsuba3/build-mitsuba
+      ninja
     '';
 
     test-mitsuba.exec = ''
-    build-mitsuba
-    cd mitsuba3/build-mitsuba
-    pytest
+      build-mitsuba
+      cd mitsuba3/build-mitsuba
+      pytest
     '';
   };
 
   enterShell = ''
-  source mitsuba3/build-mitsuba/setpath.sh
+    source mitsuba3/build-mitsuba/setpath.sh
   '';
 
   env = {
     CMAKE_CXX_COMPILER_LAUNCHER = "ccache";
+    DRJIT_LIBLLVM_PATH = "${pkgs.llvm.lib}/lib/libLLVM.so";
   };
 
   # See full reference at https://devenv.sh/reference/options/
