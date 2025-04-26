723e515bc2c0a6d4d5b127bdb2a0a4de3cdd093d
Christian Döring <christian.doering@tum.de>
2025-04-26 11:26:45 +0200

    Fixed cuda loading

--- flake.nix
+++ flake.nix
@@ -1,140 +1,146 @@
 {
-    description = "Build environment for Dr.Jit and Mitsbua3";
+  description = "Build environment for Dr.Jit and Mitsbua3";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, utils, ... }:
-        utils.lib.eachDefaultSystem (system:
-        let
-            nvidia-library-path = "/usr/lib/x86_64-linux-gnu";
-            pkgs = import nixpkgs {
-            inherit system;
-            config = {
-                allowUnfree = true;
-                cudaSupport = true;
-                config.cudaVersion = "12";
-            };
-        }; in rec {
-            scripts = {
-                configure-mitsuba = pkgs.writeShellScriptBin "configure-mitsuba" ''
-                cd $FLAKE_ROOT
-                cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S mitsuba3/ -B mitsuba3/build-mitsuba
-                '';
-                build-mitsuba = pkgs.writeShellScriptBin "build-mitsuba" ''
-                configure-mitsuba
-                cd $FLAKE_ROOT/mitsuba3/build-mitsuba
-                ninja
-                '';
-                test-mitsuba = pkgs.writeShellScriptBin "test-mitsuba" ''
-                build-mitsuba
-                pip install -r $FLAKE_ROOT/requirements.txt
-                cd $FLAKE_ROOT/mitsuba3/build-mitsuba
-                export PYTHONPATH=python:$PYTHONPATH
-                python -m pytest $@
-                '';
-                debug-mitsuba = pkgs.writeShellScriptBin "debug-mitsuba" ''
-                build-mitsuba
-                pip install -r $FLAKE_ROOT/requirements.txt
-                cd $FLAKE_ROOT/mitsuba3/build-mitsuba
-                export PYTHONPATH=python:$PYTHONPATH
-                gdb --args python -m pytest $@
-                '';
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        nvidia-library-path = "/usr/lib/x86_64-linux-gnu";
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+            config.cudaVersion = "12";
+          };
+        };
+      in rec {
+        scripts = {
+          configure-mitsuba = pkgs.writeShellScriptBin "configure-mitsuba" ''
+            cd $FLAKE_ROOT
+            cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S mitsuba3/ -B mitsuba3/build-mitsuba
+          '';
+          build-mitsuba = pkgs.writeShellScriptBin "build-mitsuba" ''
+            configure-mitsuba
+            cd $FLAKE_ROOT/mitsuba3/build-mitsuba
+            ninja
+          '';
+          test-mitsuba = pkgs.writeShellScriptBin "test-mitsuba" ''
+            build-mitsuba
+            pip install -r $FLAKE_ROOT/requirements.txt
+            cd $FLAKE_ROOT/mitsuba3/build-mitsuba
+            export PYTHONPATH=python:$PYTHONPATH
+            python -m pytest $@
+          '';
+          debug-mitsuba = pkgs.writeShellScriptBin "debug-mitsuba" ''
+            build-mitsuba
+            pip install -r $FLAKE_ROOT/requirements.txt
+            cd $FLAKE_ROOT/mitsuba3/build-mitsuba
+            export PYTHONPATH=python:$PYTHONPATH
+            gdb --args python -m pytest $@
+          '';
 
-                configure-drjit = pkgs.writeShellScriptBin "configure-drjit" ''
-                cd $FLAKE_ROOT
-                cmake -DDRJIT_ENABLE_TESTS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S mitsuba3/ext/drjit/ -B mitsuba3/build-drjit
-                '';
-                build-drjit = pkgs.writeShellScriptBin "build-drjit" ''
-                configure-drjit
-                cd $FLAKE_ROOT/mitsuba3/build-drjit
-                ninja
-                '';
-                test-drjit = pkgs.writeShellScriptBin "test-drjit" ''
-                build-drjit
-                pip install -r $FLAKE_ROOT/requirements.txt
-                cd $FLAKE_ROOT/mitsuba3/build-drjit
-                export PYTHONPATH=python:$PYTHONPATH
-                python -m pytest $@
-                '';
-                debug-drjit = pkgs.writeShellScriptBin "debug-drjit" ''
-                build-drjit
-                pip install -r $FLAKE_ROOT/requirements.txt
-                cd $FLAKE_ROOT/mitsuba3/build-drjit
-                export PYTHONPATH=python:$PYTHONPATH
-                gdb --args python -m pytest $@
-                '';
-            };
+          configure-drjit = pkgs.writeShellScriptBin "configure-drjit" ''
+            cd $FLAKE_ROOT
+            cmake -DDRJIT_ENABLE_TESTS=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -S mitsuba3/ext/drjit/ -B mitsuba3/build-drjit
+          '';
+          build-drjit = pkgs.writeShellScriptBin "build-drjit" ''
+            configure-drjit
+            cd $FLAKE_ROOT/mitsuba3/build-drjit
+            ninja
+          '';
+          test-drjit = pkgs.writeShellScriptBin "test-drjit" ''
+            build-drjit
+            pip install -r $FLAKE_ROOT/requirements.txt
+            cd $FLAKE_ROOT/mitsuba3/build-drjit
+            export PYTHONPATH=python:$PYTHONPATH
+            python -m pytest $@
+          '';
+          debug-drjit = pkgs.writeShellScriptBin "debug-drjit" ''
+            build-drjit
+            pip install -r $FLAKE_ROOT/requirements.txt
+            cd $FLAKE_ROOT/mitsuba3/build-drjit
+            export PYTHONPATH=python:$PYTHONPATH
+            gdb --args python -m pytest $@
+          '';
+        };
 
-            devShells = with pkgs; rec {
-                default = mkShell {
+        devShells = with pkgs; rec {
+          default = mkShell {
+            buildInputs = [
+              # Utility scripts
+              scripts.build-mitsuba
+              scripts.configure-mitsuba
+              scripts.test-mitsuba
+              scripts.debug-mitsuba
 
-                    buildInputs = [
+              scripts.build-drjit
+              scripts.configure-drjit
+              scripts.test-drjit
+              scripts.debug-drjit
 
-                        # Utility scripts
-                        scripts.build-mitsuba
-                        scripts.configure-mitsuba
-                        scripts.test-mitsuba
-                        scripts.debug-mitsuba
+              # Basics
+              git
+              gcc13
+              zlib
+              ninja
+              cmake
+              ccache
+              pkg-config
+              stdenv.cc.cc.lib
+              gdb
 
-                        scripts.build-drjit
-                        scripts.configure-drjit
-                        scripts.test-drjit
-                        scripts.debug-drjit
+              # CUDA
+              cudatoolkit
+              # cudaPackages.cuda_cudart
+              # cudaPackages.cuda_nvtx
+              # cudaPackages.cuda_nvrtc
 
-                        # Basics
-                        git
-                        gcc13
-                        zlib
-                        ninja
-                        cmake
-                        ccache
-                        pkg-config
-                        stdenv.cc.cc.lib
-                        gdb
+              # LLVM
+              llvmPackages_19.clang
+              llvm.lib
+              llvm.dev
 
-                        # CUDA
-                        cudatoolkit
-                        # cudaPackages.cuda_cudart
-                        # cudaPackages.cuda_nvtx
-                        # cudaPackages.cuda_nvrtc
+              # Python
+              python312Full
+              uv
 
-                        # LLVM
-                        llvmPackages_19.clang
-                        llvm.lib
-                        llvm.dev
+              # Mitsuba dependencies
+              embree
+            ];
 
-                        # Python
-                        python312Full
-                        uv
+            CMAKE_CXX_COMPILER_LAUNCHER = "ccache";
+            NIX_ENFORCE_NO_NATIVE = null;
 
-                        # Mitsuba dependencies
-                        embree
-                    ];
+            shellHook = ''
+              export FLAKE_ROOT="$PWD"
+              export CUDA_PATH=${pkgs.cudatoolkit}
 
-                    CMAKE_CXX_COMPILER_LAUNCHER = "ccache";
+              export CC="${gcc13}/bin/gcc"
+              export CXX="${gcc13}/bin/g++"
+              export PATH="${gcc13}/bin:$PATH"
 
-                    shellHook = ''
-                        export FLAKE_ROOT="$PWD"
-                        export CUDA_PATH=${pkgs.cudatoolkit}
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${nvidia-library-path}"
+              export LD_LIBRARY_PATH="${llvm.lib}/lib:$LD_LIBRARY_PATH"
+            '';
+          };
 
-                        export CC="${gcc13}/bin/gcc"
-                        export CXX="${gcc13}/bin/g++"
-                        export PATH="${gcc13}/bin:$PATH"
-
-                        export LD_LIBRARY_PATH="${nvidia-library-path}:$LD_LIBRARY_PATH"
-                        export LD_LIBRARY_PATH="${llvm.lib}/lib:$LD_LIBRARY_PATH"
-
-                    '';
-                };
-
-                test = mkShell{
-                    buildInputs = [
-                    ] ++ default.buildInputs;
-                };
-            };
-        }
+          test = mkShell {
+            buildInputs =
+              [
+              ]
+              ++ default.buildInputs;
+          };
+        };
+      }
     );
 }
